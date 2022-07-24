8801bf7a27ff2ff9229c1909c459da8cf2131aab
n8539 <huangjing8539@163.com>
2022-07-24 15:56:35 +0800

    Update Tablo.py

--- api/Tablo.py
+++ api/Tablo.py
@@ -12,13 +12,13 @@ class handler(BaseHTTPRequestHandler):
     req_url = 'https://www.tablofun.com/app/40/data/list/getAll'
     res=requests.get(req_url,timeout=5)
     data = json.loads(res.text, parse_float=str, parse_int=str)
-    self.wfile.write(data['rows'].encode())
+    self.wfile.write(json.dumps(data['rows']).encode())
     return
 
 # def req():
-#     req_url = 'https://www.tablofun.com/app/40/data/list/getAll'
-#     res=requests.get(req_url,timeout=5)
-#     data = json.loads(res.text, parse_float=str, parse_int=str)
-#     print(data['rows'])
+    # req_url = 'https://www.tablofun.com/app/40/data/list/getAll'
+    # res=requests.get(req_url,timeout=5)
+    # data = json.loads(res.text, parse_float=str, parse_int=str)
+    # print(type(json.dumps(data['rows'])))
 
 # req()
